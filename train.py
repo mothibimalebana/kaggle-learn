92f69635a9b0ef90627b6bc472553270ee41e755
unknown <mothibi.mm12@gmail.com>
2026-05-10 15:29:52 +0200

    trained model using random forest

--- train.py
+++ train.py
@@ -1,19 +1,21 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split 
+from sklearn.ensemble import RandomForestRegressor
 
 # read data and store data in Dataframe titled melbourne_data
 melbourne_data = pd.read_csv('melb_data.csv')
 
+# feature selected
 melbourne_features = ['Suburb', 'Rooms', 'Type', 'Landsize']
-print(melbourne_data.columns)
 
+# variables
 y = melbourne_data.Price
-X = melbourne_data[melbourne_features] 
+X = melbourne_data[melbourne_features]
 
+# split data into test --> 20% and train --> 80%
 train_X, test_X, train_y, test_y = train_test_split(X, y, test_size=0.2, random_state=1)
 
-print("="*30+"train"+"="*30)
-print(train_X)
+melbourne_model = RandomForestRegressor(random_state=1)
 
-print("="*30+"test"+"="*30)
-print(train_y)
+melbourne_model.fit(train_X, train_y)
+
