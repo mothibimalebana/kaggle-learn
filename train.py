502a6c560a7a8cfe4da8045b9975ebc21ce73429
unknown <mothibi.mm12@gmail.com>
2026-05-10 15:25:06 +0200

    split data, test_size=0.2

--- train.py
+++ train.py
@@ -1,9 +1,19 @@
 import pandas as pd
+from sklearn.model_selection import train_test_split 
 
 # read data and store data in Dataframe titled melbourne_data
 melbourne_data = pd.read_csv('melb_data.csv')
 
-# print summary of data 
-melbourne_data.describe()
+melbourne_features = ['Suburb', 'Rooms', 'Type', 'Landsize']
+print(melbourne_data.columns)
 
-print(melbourne_data.describe())
+y = melbourne_data.Price
+X = melbourne_data[melbourne_features] 
+
+train_X, test_X, train_y, test_y = train_test_split(X, y, test_size=0.2, random_state=1)
+
+print("="*30+"train"+"="*30)
+print(train_X)
+
+print("="*30+"test"+"="*30)
+print(train_y)
