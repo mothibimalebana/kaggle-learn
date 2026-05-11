733591792dbce601f16e129938af9da6100d596e
unknown <mothibi.mm12@gmail.com>
2026-05-11 19:46:51 +0200

    completed: summary functions and maps tutorial

--- pandas_intro.py
+++ pandas_intro.py
@@ -2,7 +2,11 @@ import pandas as pd
 
 df = pd.read_csv('winemag-data-130k-v2.csv', index_col=0)
 
-review_points_mean = df.points
-print(review_points_mean)
+review_points_mean = df.points.mean()
 
-df.points.map(lam)
+def remean_points(row):
+    row.points = row.points - review_points_mean
+    return row
+
+df_remean = df.apply(remean_points, axis='columns')
+print(df_remean)
