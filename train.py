78800f0134fdd814121f15463a4d91e18bc0e88c
unknown <mothibi.mm12@gmail.com>
2026-05-14 10:40:24 +0200

    Method 2: mae of 170821.47

--- train.py
+++ train.py
@@ -3,9 +3,11 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_absolute_error
 from sklearn.ensemble import RandomForestRegressor
 from model_ev import model_eval_mae
+from sklearn.impute import SimpleImputer
+
 
 melb_df = pd.read_csv('melb_data.csv', index_col=False)
-
+my_imputer = SimpleImputer()
 # Feature selection
 features = ['Rooms', 'Distance', 'Postcode', 'Bedroom2', 'Bathroom', 'Car', 'Landsize', 'Lattitude', 'Longtitude', 'BuildingArea']
 
@@ -19,8 +21,13 @@ train_X, test_X, train_y, test_y = train_test_split(X, y, train_size=0.8, random
 # Missing Values: Method 1
 missing_value_columns = [col for col in train_X.columns if train_X[col].isnull().any()]
 
-train_X = train_X.drop(missing_value_columns, axis=1)
-test_X = test_X.drop(missing_value_columns, axis=1)
+imputed_train_X = pd.DataFrame(my_imputer.fit_transform(train_X))
+imputed_test_X = pd.DataFrame(my_imputer.fit_transform(test_X))
 
-model_mae = model_eval_mae(train_X, test_X, train_y, test_y)
+imputed_train_X.columns = train_X.columns
+imputed_test_X.columns = test_X.columns
+print(imputed_train_X)
+print(imputed_test_X)
+
+model_mae = model_eval_mae(imputed_train_X, imputed_test_X, train_y, test_y)
 print(model_mae)
