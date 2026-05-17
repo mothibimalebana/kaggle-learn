a5fb9b77eca5552f81ee8af9318eb67247bfdfe8
unknown <mothibi.mm12@gmail.com>
2026-05-17 11:18:52 +0200

    applied one hot encoder to categorical columns

--- model_ev.py
+++ model_ev.py
@@ -3,11 +3,7 @@ from sklearn.ensemble import RandomForestRegressor
 
 # Function to compare different methods
 def model_eval_mae(train_X, test_X, train_y, test_y):
-    """
-    input: train_X, test_X, train_y, test_y
-    output: mean_absolute_error
-    """
-    model = RandomForestRegressor(random_state=0)
+    model = RandomForestRegressor(n_estimators=100, random_state=0)
     model.fit(train_X, train_y)
-    pred_y = model.predict(test_X)
-    return mean_absolute_error(test_y,pred_y)
+    preds = model.predict(test_X)
+    return mean_absolute_error(test_y, preds)
--- train.py
+++ train.py
@@ -2,36 +2,40 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.impute import SimpleImputer
 from sklearn.preprocessing import OneHotEncoder
+from model_ev import model_eval_mae
 
 
 my_imputer = SimpleImputer()
+OH_encoder = OneHotEncoder(handle_unknown='ignore', sparse_output=False)
 
 # Read data
 df = pd.read_csv('melb_data.csv')
 
-# Find columns with int or float dtype
-num_cols = [col for col in df.columns if ((df[col].dtype == 'int') or df[col].dtype == 'float')]
-
-# find columns with categorical values
-categorical_cols = [col for col in df.columns if ((df[col].dtype == 'str') and (df[col].nunique() <= 10))]
-
-
-refined_cols = num_cols + categorical_cols
-
-df_num_cols = df[refined_cols]
-
 # Define predictor and predictors
 y = df.Price
-X = df_num_cols.drop(['Price'], axis='columns')
+X = df.drop(['Price'], axis='columns')
+
+# Find columns with int or float dtype
+num_cols = [col for col in X.columns if ((X[col].dtype == 'int') or X[col].dtype == 'float')]
+
+# find columns with categorical values
+low_cardinality_cols = [col for col in X.columns if ((X[col].dtype == 'str') and (X[col].nunique() <= 10))]
+
+
+refined_cols = num_cols + low_cardinality_cols
+
+X = df[refined_cols]
 
 # List of predictors with missing values
 missing_value_cols = [col for col in X.columns if X[col].isnull().any()]
-print(missing_value_cols)
-X = X.drop(missing_value_cols, axis='columns')
 
+X = X.drop(missing_value_cols, axis='columns')
 
 # Split data into training vs test data
 train_X, test_X, train_y, test_y = train_test_split(X, y, test_size=0.2, train_size=0.8, random_state=0)
 
-# For simplicity we drop missing columns
-print(train_X[categorical_cols])
+# Encoding categorical variables
+OH_cols_train = pd.DataFrame(OH_encoder.fit_transform(train_X[low_cardinality_cols]))
+OH_cols_test = pd.DataFrame(OH_encoder.transform(test_X[low_cardinality_cols]))
+
+print(OH_cols_train)
